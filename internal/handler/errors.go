6dfc3d07705d696e68d9f57f28b7742f6e5f2456
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-08-12 06:38:06 +0700

    add new error types

--- internal/handler/errors.go
+++ internal/handler/errors.go
@@ -4,9 +4,9 @@ import "errors"
 
 var (
 	ErrInternal        = errors.New("internal error")
-	ErrExternalService = errors.New("external service return an unexpected error")
+	ErrExternalService = errors.New("external service return an error. may be caused invalid request")
 	ErrBadRequest      = errors.New("bad request: please use correct command syntax. Get help by typing /command-name help")
 	ErrNotFound        = errors.New("entities not found, please try again")
 	ErrValidation      = errors.New("validation error. Try sending correct and valid data then try again")
-	ErrUnauthorized    = errors.New("request unauthorized")
+	ErrUnauthorized    = errors.New("request unauthorized. Maybe try using login command first")
 )
