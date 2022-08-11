edda679ed8825e5bc21daf7b51c236161625c621
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-08-12 06:38:49 +0700

    add new method

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -93,11 +93,104 @@ func (h *Handler) LoginHandler() handlers.Response {
 		case usecase.ErrValidation:
 			return replyErrorMessageToUser(b, ctx, ErrValidation)
 		case usecase.ErrExternalService:
-			return replyErrorMessageToUser(b, ctx, ErrExternalService)
+func (h *Handler) GetAbsentFormHandler() handlers.Response {
+	paramLength := 1
+
+	return func(b *gotgbot.Bot, c *ext.Context) error {
+		ctx, err := h.authUsecase.CreateContextForUser(c.EffectiveSender.Id())
+		if errors.Is(err, usecase.ErrInternal) {
+			logrus.Error(err)
+			return replyErrorMessageToUser(b, c, ErrInternal)
+		}
+
+		params, err := extractParamsFromText(c, paramLength)
+		if err != nil {
+			_, err := replyGetAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		formID, err := strconv.ParseInt(params[0], 10, 64)
+		if err != nil {
+			return replyErrorMessageToUser(b, c, ErrBadRequest)
+		}
+
+		absentForm, err := h.absentUsecase.GetAbsentFormByID(ctx, formID)
+		switch err {
+		default:
+			logrus.Error(err)
+			return replyErrorMessageToUser(b, c, ErrInternal)
 		case usecase.ErrUnauthorized:
-			return replyErrorMessageToUser(b, ctx, ErrUnauthorized)
+			return replyErrorMessageToUser(b, c, ErrUnauthorized)
 		case nil:
-			return replySuccessMessageToUser(b, ctx, "login success")
+			return replyTextMessageToUser(b, c, absentForm.ToText())
+		}
+	}
+}
+
+func (h *Handler) FillAbsentFormHandler() handlers.Response {
+	return func(b *gotgbot.Bot, c *ext.Context) error {
+		ctx, err := h.authUsecase.CreateContextForUser(c.EffectiveSender.Id())
+		if err != nil {
+			logrus.Error(err)
+			return replyErrorMessageToUser(b, c, ErrInternal)
+		}
+
+		input := removeCommandFromText(c.Message.Text)
+		if input == "" || input == "help" {
+			_, err := replyFillAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		params := splitInputToParams(input)
+		if len(params) < 2 {
+			_, err := replyFillAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		status, err := model.StringToStatus(params[1])
+		if err != nil {
+			_, err := replyFillAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		if status != model.StatusPresent && len(params) != 3 {
+			_, err := replyFillAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		formID, err := strconv.ParseInt(params[0], 10, 64)
+		if err != nil {
+			_, err := replyFillAbsentFormCommandHelp(b, c)
+			return err
+		}
+
+		var execuseReason string
+		if len(params) == 3 {
+			execuseReason = params[2]
+		}
+
+		payload := &model.FillAbsentPayload{
+			Status:        string(status),
+			ExecuseReason: execuseReason,
+		}
+
+		fmt.Println(payload.Status)
+		fmt.Println(payload.ExecuseReason)
+
+		err = h.absentUsecase.FillAbsentForm(ctx, formID, payload)
+		switch err {
+		default:
+			logrus.Warn(err)
+			return replyTextMessageToUser(b, c, err.Error())
+		case usecase.ErrInternal:
+			logrus.Error(err)
+			return replyErrorMessageToUser(b, c, ErrInternal)
+		case usecase.ErrValidation:
+			return replyErrorMessageToUser(b, c, ErrValidation)
+		case usecase.ErrUnauthorized:
+			return replyErrorMessageToUser(b, c, ErrUnauthorized)
+		case nil:
+			return replyTextMessageToUser(b, c, "Ok.")
 		}
 	}
 }
